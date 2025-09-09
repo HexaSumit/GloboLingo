d72efef3237dd6d225c83a7b175ad6b5003635f1
Sumit Bhardwaj <sumitbhardwaj78368@gmail.com>
2025-09-09 12:06:05 +0530

    feat:Add routing for all pages in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,69 @@
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom'
 import './App.css'
+import Learn from './pages/Learn'
+import LanguageSelector from './components/LanguageSelector'
+import Navbar from './components/Navbar'
+import About from './pages/About'
+import Login from './pages/Login'
+import Quiz from './pages/Quiz'
+import Result from './pages/Result'
 
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<>
+    <Navbar />
+    <LanguageSelector />
+    </>
+  },
+  {
+    path:'/learn',
+    element:<>
+    <Navbar />
+    <Learn />
+    </>
+  },
+  {
+    path:'/about',
+    element:<>
+    <Navbar />
+    <About />
+    </>
+  },
+  {
+    path:'/about',
+    element:<>
+    <Navbar />
+    <About />
+    </>
+  },
+   {
+    path:'/quiz',
+    element:<>
+    <Navbar />
+    <Quiz />
+    </>
+  },
+  {
+    path:'/result',
+    element:<>
+    <Navbar />
+    <Result />
+    </>
+  },
+  {
+    path:'/login',
+    element:<Login />
+  },
+])
 function App() {
 
   return (
     <>
-    <div>hello world</div>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
     </>
   )
 }
